b29d0fa7e611e6e3bf2f3c0fef65892910d8708b
HolgadoJairoDavid <jairodavidholgado@gamil.com>
2022-12-11 09:39:58 -0300

    Second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -237,7 +237,8 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra.length > 1){
     return 'Dato incorrecto';
-  } if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+  } if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ||
+  letra === 'A' || letra === 'E' || letra === 'I' || letra === 'O' || letra === 'U') {
     return 'Es vocal';
   } return 'Dato incorrecto';
 }
